30ffa644a5e9b27364b418602356916e93b4cbf6
Nyasha Chizampeni <nchizampeni@gmail.com>
2023-07-02 02:58:02 +0200

    save

--- evaluation/urls.py
+++ evaluation/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
-from evaluation.views import EvaluationViewSet, TagViewSet, AttachmentViewSet, QuestionViewSet, ResponseViewSet, ParticipantViewSet
+from evaluation.views import EvaluationViewSet, TagViewSet, AttachmentViewSet, QuestionViewSet, ResponseViewSet, ParticipantViewSet, AnswerViewSet
 
 router = routers.DefaultRouter()
 router.register(r'evaluations', EvaluationViewSet)
@@ -9,6 +9,8 @@ router.register(r'attachments', AttachmentViewSet)
 router.register(r'questions', QuestionViewSet)
 router.register(r'responses', ResponseViewSet)
 router.register(r'participants', ParticipantViewSet)
+router.register(r'answers', AnswerViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
